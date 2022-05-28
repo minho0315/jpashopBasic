d0dc79a0d1e482c1f50947c010a03e5da74fd3dd
minho0315 <dlalsgh369@naver.com>
2022-05-28 13:32:58 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -21,9 +22,13 @@ public class JpaMain {
 
         try {
 
-            Order order = em.find(Order.class, 1L);
-            Member member = order.getMember();
+            Order order = new Order();
+            em.persist(order);
 
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
 
             tx.commit();
         } catch (Exception e) {
