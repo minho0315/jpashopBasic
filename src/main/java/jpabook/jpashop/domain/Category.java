9e7496ab90e1742f439282d31ee9214ab8b1fe80
minho0315 <dlalsgh369@naver.com>
2022-06-08 23:57:25 +0900

    실전 예제 5 - 연관관계 관리

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 public class Category extends BaseEntity {
 
@@ -12,7 +14,7 @@ public class Category extends BaseEntity {
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
@@ -25,4 +27,7 @@ public class Category extends BaseEntity {
             inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
     )
     private List<Item> items = new ArrayList<>();
+
+    public Category() {
+    }
 }
